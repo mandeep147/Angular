a4a3683764466668028e718021bef791021dccc2
mandeep147 <mndpkaur14@gmail.com>
2020-03-28 19:10:18 -0500

    use css file instead

--- src/app/image-component/image.component.ts
+++ src/app/image-component/image.component.ts
@@ -3,16 +3,7 @@ import {Component, Input} from "@angular/core";
 @Component({
   selector: 'app-image',
   template: '<div class="thumbnail"><img src="{{imageUrl}}"/></div>',
-  styles: [
-    `.thumbnail {
-      width: 100px;
-      height: 100px;
-    }
-    img {
-      height: 100%;
-      width: auto;
-    }`
-  ]
+  styleUrls: ['./image.component.css']
 })
 export class ImageComponent {
   @Input() imageUrl;
