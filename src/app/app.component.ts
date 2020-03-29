c6e771de9fbd750758994466dfca9f2e47d1bfcb
mandeep147 <mndpkaur14@gmail.com>
2020-03-28 19:58:27 -0500

    use bootstrap styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  name = "Mandeep";
+  title = 'The Dating App';
   items = [
     'https://cdn.pixabay.com/photo/2018/10/17/11/04/dog-3753706_1280.jpg',
     'https://cdn.pixabay.com/photo/2017/05/30/23/26/baby-alaskan-malamute-2358585_1280.jpg',
